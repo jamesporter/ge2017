9d72c6753987edfcb51a18995b19854350c7b7a2
James Porter <james@amimetic.co.uk>
2017-06-04 14:24:36 +0100

    should have all data/metadata for a 2015 baseline visualisation, which I can then update with polls/swing etc

--- ge-impact/src/App.js
+++ ge-impact/src/App.js
@@ -3,15 +3,22 @@ import logo from './logo.svg';
 import './App.css';
 import data from "./data/data.json"
 import main2015 from "./data/main2015.json"
+import meta from "./data/meta.json"
+
+const { parties } = meta;
+const partyKeys = Object.keys(parties);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>GE Impact</h1>
+        <h1>General Election Impact 2017</h1>
 
         {main2015.map((d, idx) => <div key={idx}>
           <h2>{d["Constituency Name"]}</h2>
+          {partyKeys.map((p, pIdx) => <div key={pIdx}>
+            <p style={{color: parties[p].colour}}>{parties[p].name} {d[p]}</p>
+          </div>)}
           <pre>{JSON.stringify(d, null, 2)}</pre>
         </div>)}
       </div>
