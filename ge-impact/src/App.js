7274d27b94eac5277e4285beebeae51e534ec2c8
James Porter <james@amimetic.co.uk>
2017-06-04 15:21:05 +0100

    some basic stylings/layouts

--- ge-impact/src/App.js
+++ ge-impact/src/App.js
@@ -12,15 +12,19 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>General Election Impact 2017</h1>
+        <div className="controls">
+          <h1>General Election Impact 2017</h1>
+        </div>
 
-        {main2015.map((d, idx) => <div key={idx}>
-          <h2>{d["Constituency Name"]}</h2>
-          {partyKeys.map((p, pIdx) => <div key={pIdx}>
-            <p style={{color: parties[p].colour}}>{parties[p].name} {d[p]}</p>
+        <div className="view">
+          {main2015.map((d, idx) => <div key={idx}>
+            <h2>{d["Constituency Name"]}</h2>
+            {partyKeys.map((p, pIdx) => <div key={pIdx}>
+              <p style={{color: parties[p].colour}}>{parties[p].name} {d[p]}</p>
+            </div>)}
+            <pre>{JSON.stringify(d, null, 2)}</pre>
           </div>)}
-          <pre>{JSON.stringify(d, null, 2)}</pre>
-        </div>)}
+        </div>
       </div>
     );
   }
