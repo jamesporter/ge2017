d630058aa525cc14bc35a3d08c6862bb42e3a5b4
James Porter <james@amimetic.co.uk>
2017-06-04 13:45:34 +0100

    outputing some workable data... should shortly be able to see baseline for 2015

--- ge-impact/src/App.js
+++ ge-impact/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from "./data/data.json"
+import main2015 from "./data/main2015.json"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>GE Impact</h1>
+
+        {main2015.map((d, idx) => <div key={idx}>
+          <h2>{d["Constituency Name"]}</h2>
+          <pre>{JSON.stringify(d, null, 2)}</pre>
+        </div>)}
       </div>
     );
   }
